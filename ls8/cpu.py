9cda734225c84232ac463d588c6ceabb3855d836
Vyraal1 <kevin.afable@gmail.com>
2020-03-23 22:22:35 -0400

    feat: PRN/HLT instruction

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,17 +8,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [None] * 8
-        self.reg = []
+        # check the spec
+        self.ram = [0] * 256
+        self.reg = [0] * 8
         self.pc = 0
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
-
         # For now, we've just hardcoded a program:
-
         program = [
             # From print8.ls8
             0b10000010,  # LDI R0,8
@@ -35,7 +33,6 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
@@ -61,4 +58,27 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        self.pc = 0
+        running = True
+
+        HLT = 1
+        PRN = 47
+        while running:
+            command = self.ram[self.pc]
+
+            if command == HLT:
+                running = False
+                self.pc += 1
+            elif command == PRN:
+                self.pc += 2
+            else:
+                print(f"Unkown instruction: {command}")
+                sys.exit(1)
+
+            self.pc += 1
+
+    def ram_read(self, pc):
+        pass
+
+    def ram_write(self):
         pass
