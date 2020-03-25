75109f758787d52315f5adaee79c3eb25f64b73a
Kevin <kevin.afable@gmail.com>
2020-03-25 11:04:38 -0400

    feat: loading program from file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,20 +16,16 @@ class CPU:
     def load(self):
         """Load a program into memory."""
         address = 0
-        # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        if len(sys.argv) != 2:
+            print('Incorrect Usage Error: python ls8.py [filename]')
+            sys.exit(1)
+
+        # open the file
+        filename = sys.argv[1]
+        with open(filename) as program:
+            for address, instruction in enumerate(program):
+                self.ram[address] = instruction
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -42,14 +38,8 @@ class CPU:
     def trace(self):
         """ Handy function to print out the CPU state. You might want to call this from run() if you need help debugging."""
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            # self.fl,
-            # self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: {self.pc} | {self.ram_read(self.pc)} {self.ram_read(self.pc + 1)} {self.ram_read(self.pc + 2)} |", end='')
 
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
@@ -62,23 +52,33 @@ class CPU:
         running = True
 
         HLT = 1
-        PRN = 47
+        LDI = 130
+        PRN = 71
+
         while running:
-            command = self.ram[self.pc]
+            command = self.ram_read(self.pc)
 
             if command == HLT:
                 running = False
-                self.pc += 1
+                sys.exit(0)
+            elif command == LDI:
+                register = self.ram_read(self.pc + 1)
+                value = self.ram_read(self.pc + 2)
+                self.reg[register] = value
+                self.pc += 3
             elif command == PRN:
+                reg = self.ram_read(self.pc+1)
+                print(self.reg[reg])
                 self.pc += 2
             else:
-                print(f"Unkown instruction: {command}")
+                self.trace()
+                print(f"Unknown instruction: {command} at pc {self.pc}")
                 sys.exit(1)
 
-            self.pc += 1
+    def ram_read(self, mar):
+        ''' Looks in memory and returns the value found there. Normally MAR (Memory Address Register) is an internal register in the CPU that stores the address to look at, however in this instance, the function just takes the MAR to read from and returns the value it finds there.'''
+        return self.ram[mar]
 
-    def ram_read(self, pc):
-        pass
-
-    def ram_write(self):
-        pass
+    def ram_write(self, mar, mdr):
+        ''' Accepts a mar and mdr (Memory Data Register) and it will write the value of the MDR at the given MAR. Like ram_read(), these registers are internally within a CPU, but instead they will be used as parameter names. '''
+        self.ram[mar] = mdr
